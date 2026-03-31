c0eee3819767b496481689f2d9540a27a4a5c3ba
haqqulovsardor51@gmail.com <your_email@example.com>
2026-03-31 12:39:35 +0500

    Initial commit

---
+++ src/main/java/sfera/eduportal2/EduPortal2Application.java
@@ -0,0 +1,13 @@
+package sfera.eduportal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPortal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPortal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sfera/eduportal2/EduPortal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sfera.eduportal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPortal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
