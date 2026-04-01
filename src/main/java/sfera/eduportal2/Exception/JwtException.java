e35200b00221c8f15cf59b6094ff1d2f8bfc1ec3
USER <nurbekf11@gmail.com>
2026-04-01 15:33:36 +0500

    Nurik sila qildi

--- src/main/java/sfera/eduportal2/Exception/JwtException.java
+++ src/main/java/sfera/eduportal2/Exception/JwtException.java
@@ -1,6 +1,5 @@
 package sfera.eduportal2.Exception;
 
-import sfera.eduportal2.entity.Payload.ApiResponse;
 
 public class JwtException extends RuntimeException {
     public JwtException( String message) {
